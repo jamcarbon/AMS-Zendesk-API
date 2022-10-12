7b405bfc852c84b2242d859456de20150517b977
jamcarbon <jamcarbon@hotmail.com>
2022-10-12 18:04:12 +1300

    update

---
+++ infrastructure/deploy.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+aws cloudformation describe-stacks
+
+aws cloudformation validate-template --template-body file://C:\Users\jamca\OneDrive\Documents\Github\Repositories\AMS-Zendesk-API\infrastructure\stack.yaml
+
+aws cloudformation create-stack --stack-name Zendesk-AMS-integration-test --template-body file://C:\Users\jamca\OneDrive\Documents\Github\Repositories\AMS-Zendesk-API\infrastructure\stack.yaml --capabilities CAPABILITY_NAMED_IAM
+
+aws cloudformation delete-stack --stack-name Zendesk-AMS-integration-test
+
+
+
+
+
+aws cloudformation create-stack --stack-name minimal-example --template-body file://C:\Users\jamca\OneDrive\Documents\Github\Repositories\AMS-Zendesk-API\infrastructure\stack.yaml --capabilities CAPABILITY_NAMED_IAM
+
+aws cloudformation create-stack \
+  --stack-name minimal-example \
+  --capabilities CAPABILITY_NAMED_IAM \
+  --template-body file://minimal-example.yml
+
+
+
+aws cloudformation describe-stack-events --stack-name minimal-example
+
+aws cloudformation delete-stack --stack-name minimal-example
+
+aws cloudformation update-stack --stack-name minimal-example --template-body file://C:\Users\jamca\OneDrive\Documents\Github\Repositories\AMS-Zendesk-API\infrastructure\stack.yaml --capabilities CAPABILITY_NAMED_IAM
