47e8f00bc56e3124209e2a49fd5be27df0161ce4
jamcarbon <jamcarbon@hotmail.com>
2022-10-25 15:17:38 +1300

    update

--- Step1_function.py
+++ Step1_function.py
@@ -62,42 +62,20 @@ def lambda_handler(event, context):
             "ticket_tags": ticket_json['ticket']["ticket_tags"]
         }
         
-        ticket_status = ticket_json['ticket']["ticket_status"]
-        ticket_type = ticket_json['ticket']["ticket_type"]
-        ticket_priority = ticket_json['ticket']["ticket_priority"]
-        ticket_requester_name = ticket_json['ticket']["ticket_requester_name"]
-        ticket_service = ticket_json['ticket']["ticket_service"]
-        ticket_impact = ticket_json['ticket']["ticket_impact"]
-        created_at = ticket_json['ticket']["created_at"]
-        description = ticket_json['ticket']["description"]
-        ticket_id= ticket_json['ticket']["id"]
-        ticket_subject = ticket_json['ticket']["ticket_subject"]
-        updated_at = ticket_json['ticket']["updated_at"]
-        ticket_url = ticket_json['ticket']["ticket_url"]
-        public_comment = ticket_json['ticket']["public_comment"]
-        latest_public_comment = ticket_json['ticket']["latest_public_comment"]
-        ticket_tags = ticket_json['ticket']["ticket_tags"]
-
-        i = {
-                "reply": "The ticket has been updated sucesfully.",
-                "ticket_id": ticket_id
-            }
-        
-        reply = "The ticket", ticket_id, "has been updated sucesfully."
+        reply = "The ticket", ticket_data["ticket_id"], "has been updated sucesfully."
         reply1 = "The ticket has been updated sucesfully."
         
-        print(ticket_id)
-        print("all public comments", public_comment)
-        print("latest comment", latest_public_comment)
         print("ticket_data", ticket_data)
 
         ams_caseID = describe_cases(ticket_data)
 
         add_communication_to_case(ams_caseID, ticket_data)
         
+        reply2 = "The AMS ticket #", ams_caseID,  "has been updated sucesfully."
+
         return {
             'statusCode': 200,
-            'body': reply1
+            'body': reply2
         }
     
     except botocore.exceptions.ClientError as error:
